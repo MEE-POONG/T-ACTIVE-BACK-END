bdb1ac068e2f1b4f4dfd2f4891f93655e1a2c54a
Ploy <117616170+yaminP@users.noreply.github.com>
2023-01-03 09:59:56 +0700

    nav

--- components/Sidebar/TheSlideNav.jsx
+++ components/Sidebar/TheSlideNav.jsx
@@ -65,7 +65,7 @@ export default function TheSlideNav() {
 
             <Dropdown bsPrefix="nav-item">
               <Dropdown.Toggle className={
-                  asPath === "/homee" || asPath === "/about" || asPath === "/contact"
+                  asPath === "/hometop" || asPath === "/homedetail" || asPath === "/about" || asPath === "/contact"
                   ? "nav-item nav-link active" 
                   : "nav-item nav-link"} id="dropdown-custom-components" >
                   <i className="me-2">
