83c29e22d5460dc1cf71d818bc1715bf62ba4aa6
devilzeros <devilzeros@outlook.co.th>
2023-02-27 19:39:50 +0700

    ำหะ

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,43 @@
 import Head from 'next/head';
+import { useState, useEffect } from 'react';
 import IndexPage from "components/layouts/IndexPage";
-import ResultNumber from 'container/Dashboard/ResultNumber';
-import PeakGraph from 'container/Dashboard/PeakGraph';
-import RecentSalse from 'container/Dashboard/RecentSalse';
-import NewListing from 'container/Dashboard/NewListing';
-import { Container } from 'react-bootstrap';
+import { Container, Image, Table, Button, Form, Modal } from 'react-bootstrap';
+import useAxios from 'axios-hooks';
+import { FaReply, FaPlus, FaEdit, FaTrash } from 'react-icons/fa';
+import axios from 'axios'
+import FormData from 'form-data';
+import { CKEditor } from "ckeditor4-react";
 export default function HomePage() {
   return (
     < >
       <Head>
-        <title>T-ACTIVE</title>
+        <title>สวัสผู้ดูแลระบบ</title>
         <meta
           name="description"
           content="I2AROBOT 2"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <Container>
-      
-    </Container>
+      <Container fluid className=" pt-4 px-4">
+        <div className="bg-secondary rounded shadow p-4">
+          <h5>ยินดีต้อนรับ</h5>
+          <div className="table-responsive w-100">
+
+          </div> <br />
+          <div className="d-flex align-items-center justify-content-between mb-4">
+            <h5 className="mb-0 w-m-max me-2">ข้อมูล</h5>
+            <Button variant="dark">
+              <FaPlus />
+            </Button>
+          </div>
+
+          <div className=" w- d-flex align-items-center border-bottom py-2">
+            <div className="table-responsive w-100">
+
+            </div>
+          </div>
+        </div>
+      </Container>
     </ >
   );
 }
